057898a35a9ddd470bdfe261de3c083da8ce4319
Darren Ballinger <darren@braxis-ltd.com>
2012-11-20 20:35:58 +0000

    added coverage

--- config/testacular-gradle.conf.js
+++ config/testacular-gradle.conf.js
@@ -5,6 +5,7 @@ reporters = ['coverage'];
 files = [
   JASMINE,
   JASMINE_ADAPTER,
+  'app/lib/underscore-1.3.3.js',
   'app/lib/angular/angular.js',
   'app/lib/angular/angular-*.js',
   'test/lib/angular/angular-mocks.js',
@@ -13,7 +14,7 @@ files = [
 ];
 
 preprocessors = {
-  '**/app/js/*.js': 'coverage'
+    '**/app/js/**/*.js':'coverage'
 };
 
 autoWatch = false;
@@ -22,10 +23,8 @@ browsers = ['Chrome'];
 
 singleRun = true;
 
-reporters = ['junit'];
-
 junitReporter = {
-  outputFile: 'build/testacular-unit.xml',
+  outputFile: 'test_out/unit.xml',
   suite: 'unit'
 };
 
@@ -33,3 +32,39 @@ coverageReporter = {
   type : 'html',
   dir : 'coverage/'
 };
+
+//basePath = '../';
+//
+//reporters = ['coverage'];
+//
+//files = [
+//  JASMINE,
+//  JASMINE_ADAPTER,
+//  'app/lib/angular/angular.js',
+//  'app/lib/angular/angular-*.js',
+//  'test/lib/angular/angular-mocks.js',
+//  'app/js/**/*.js',
+//  'test/unit/**/*.js'
+//];
+//
+//preprocessors = {
+//  '**/app/js/*.js': 'coverage'
+//};
+//
+//autoWatch = false;
+//
+//browsers = ['Chrome'];
+//
+//singleRun = true;
+//
+//reporters = ['junit'];
+//
+//junitReporter = {
+//  outputFile: 'build/testacular-unit.xml',
+//  suite: 'unit'
+//};
+//
+//coverageReporter = {
+//  type : 'html',
+//  dir : 'coverage/'
+//};
